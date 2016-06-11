0cd6b96d2bb7263541e2733aa98ee9cd84b9a956
Ryan Flach <ryanflach@users.noreply.github.com>
2016-06-10 21:10:45 -0600

    [WIP] Worked on first part of iteration 3 - lots of methods and tests

--- lib/statewide_test_repository.rb
+++ lib/statewide_test_repository.rb
@@ -1,4 +1,4 @@
-require_relative 'enrollment'
+require_relative 'statewide_test'
 require 'csv'
 
 class StatewideTestRepository
@@ -16,37 +16,97 @@ class StatewideTestRepository
     tests[district_name.upcase]
   end
 
-  def find_headers(filename)
-    CSV.read(filename, headers: true, header_converters: :symbol).headers
+  def grade_levels
+    [:third_grade, :eigth_grade]
+  end
+
+  def base_data(row)
+    {:name => row[:location], :year => row[:timeframe].to_i, :percent => row[:data].to_f}
   end
 
   def load_data(data_source)
     data_source.values[0].values.each_with_index do |filename, index|
-      headers = find_headers
       CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+        data = base_data(row)
         name, year, percent = row[:location], row[:timeframe], row[:data]
+        data_type = data_source.values[0].keys[index]
         existing = find_by_name(name)
-        if headers.include?(:race_ethnicity)
-          data = {:race => row[:race_ethnicity], :name => name,
-                  :subject => data_source.values[0].keys[index],
-                  :year => year, :percent => percent, :existing => existing}
-          merge_test_data(data)
+        # binding.pry
+        if grade_levels.include?(data_type)
+          data[:subject], data[:grade] = row[:score].downcase.to_sym, data_type
+          add_grade_data(data, existing)
         else
-          merge_grade_data(existing, name, row[:score], year, percent)
+          data[:race], data[:subject], = row[:race_ethnicity].downcase.to_sym, data_type
+          add_test_results(data, existing)
         end
       end
     end
   end
 
-  def merge_test_data(data)
-    if data[:existing].nil?
-      statewide_data = {:name => data[:name],
-                        data[:subject] => {data[:race] =>
-                        {data[:year] => data[:percent]}}}
-      add_testing_data(StatewideTest.new(statewide_data)
-    elsif data[:existing].test_data.has_key?(data[:subject])
-      data[:existing].test_data[data[:subject]].merge!({data[:race] => })
+  def add_grade_data(data, existing)
+    statewide_data = {:name => data[:name],
+                      data[:grade] =>
+                     {data[:year] => {data[:subject] => data[:percent]}}}
+    if existing.nil?
+      add_testing_data(StatewideTest.new(statewide_data))
+    elsif has_grade_and_year(existing, data[:grade], data[:year])
+      existing.year_data(data[:grade], data[:year]).merge!({data[:subject] => data[:percent]})
+    else
+      existing.grade_data(data[:grade]).merge!({data[:year] => {data[:subject] => data[:percent]}})
+    end
   end
+
+  def add_test_results(data, existing)
+    statewide_data = {:name => data[:name],
+                      data[:race] =>
+                     {data[:year] => {data[:subject] => data[:percent]}}}
+    if existing.nil?
+      # binding.pry
+      add_testing_data(StatewideTest.new(statewide_data))
+    elsif has_race_and_year(existing, data[:race], data[:year])
+      # binding.pry
+      existing.year_data(data[:race], data[:year]).merge!({data[:subject] => data[:percent]})
+    else
+      # binding.pry
+      existing.race_data(data[:race]).merge!({data[:year] => {data[:subject] => data[:percent]}})
+    end
+  end
+
+  def has_grade(test_object, grade)
+    test_object.grade_data(grade)
+  end
+
+  def has_year(test_object, grade_or_race, year)
+    test_object.year_data(grade_or_race, year)
+  end
+
+  def has_grade_and_year(object, grade, year)
+    has_grade(object, grade) && has_year(object, grade, year)
+  end
+
+  def has_race(test_object, race)
+    test_object.race_data(race)
+  end
+
+  def has_race_and_year(object, race, year)
+    has_race(object, race) && has_year(object, race, year)
+  end
+  # def add_test_results(data, existing)
+  #   statewide_data = {:name => data[:name],
+  #                     :}
+  #   if existing.nil?
+  #     add_testing_data
+  # end
+
+  # def merge_test_data(data)
+  #   if data[:existing].nil?
+  #     statewide_data = {:name => data[:name],
+  #                       data[:subject] => {data[:race] =>
+  #                       {data[:year] => data[:percent]}}}
+  #     add_testing_data(StatewideTest.new(statewide_data)
+  #   elsif data[:existing].test_data.has_key?(data[:subject])
+  #     data[:existing].test_data[data[:subject]].merge!({data[:race] => })
+  # end
 
 
   # def enrollment_types
---
+++ test/statewide_test_respository_test.rb
@@ -0,0 +1,178 @@
+require_relative 'test_helper'
+require_relative '../lib/statewide_test_repository'
+
+class StatewideTestRepositoryTest < Minitest::Test
+  def test_it_initializes_with_an_empty_hash_by_default
+    # skip
+    str = StatewideTestRepository.new
+    expected = {}
+    assert_equal expected, str.tests
+  end
+
+  def test_it_can_add_test_data_and_store_statewide_test_objects
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new(:name => "Colorado")
+    str.add_testing_data(st)
+
+    expected = {"COLORADO" => st}
+    assert_equal expected, str.tests
+  end
+
+  def test_it_can_find_statewide_test_objects_by_name
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new(:name => "Colorado")
+    str.add_testing_data(st)
+
+    expected = st
+    assert_equal expected, str.find_by_name("Colorado")
+  end
+
+  def test_it_can_check_if_a_statewide_test_object_has_grade_data
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new(:name => "Colorado")
+    str.add_testing_data(st)
+
+    refute str.has_grade(st, :third_grade)
+
+    st.test_data[:third_grade] = {2010 => {:math => 0.333}}
+    assert str.has_grade(st, :third_grade)
+  end
+
+  def test_it_can_check_if_a_statewide_test_object_has_data_for_a_given_year
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new(:name => "Colorado")
+    str.add_testing_data(st)
+
+    refute str.has_year(st, :third_grade, 2010)
+
+    st.test_data[:third_grade] = {2010 => {:math => 0.333}}
+    assert str.has_year(st, :third_grade, 2010)
+  end
+
+  def test_it_can_check_if_a_statwide_test_object_has_grade_and_given_year
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new(:name => "Colorado", :third_grade => {2010 => {:math => 0.333}})
+    str.add_testing_data(st)
+
+    refute str.has_grade_and_year(st, :third_grade, 2011)
+    assert str.has_grade_and_year(st, :third_grade, 2010)
+  end
+
+  def test_it_can_add_grade_data_to_its_repository_when_object_is_new
+    # skip
+    str = StatewideTestRepository.new
+    existing = nil
+    data = {:name => "Colorado", :grade => :third_grade, :year => 2010, :subject => :math, :percent => 0.333}
+    str.add_grade_data(data, existing)
+
+    expected = {2010 => {:math => 0.333}}
+    assert_equal expected, str.find_by_name('colorado').grade_data(:third_grade)
+  end
+
+  def test_it_can_add_grade_data_to_its_repository_when_object_grade_exists
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new({:name => "Colorado", :third_grade => {2010 => {:math => 0.333}}})
+    str.add_testing_data(st)
+    existing = st
+    data = {:name => "Colorado", :grade => :third_grade, :year => 2011, :subject => :math, :percent => 0.465}
+    str.add_grade_data(data, existing)
+
+    expected = {2010 => {:math => 0.333}, 2011 => {:math => 0.465}}
+    assert_equal expected, str.find_by_name('colorado').grade_data(:third_grade)
+  end
+
+  def test_it_can_add_grade_data_to_its_repository_when_object_grade_and_year_exists
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new({:name => "Colorado", :third_grade => {2010 => {:math => 0.333}}})
+    str.add_testing_data(st)
+    existing = st
+    data = {:name => "Colorado", :grade => :third_grade, :year => 2010, :subject => :reading, :percent => 0.465}
+    str.add_grade_data(data, existing)
+
+    expected = {2010 => {:math => 0.333, :reading => 0.465}}
+    assert_equal expected, str.find_by_name('colorado').grade_data(:third_grade)
+  end
+
+  def test_it_can_if_a_statewide_object_has_race_data
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new(:name => "Colorado")
+    str.add_testing_data(st)
+
+    refute str.has_race(st, :asian)
+
+    st.test_data[:asian] = {2010 => {:math => 0.333}}
+    assert str.has_race(st, :asian)
+  end
+
+  def test_it_can_check_if_a_statewide_object_has_race_data_for_a_given_year
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new(:name => "Colorado", :asian => {2010 => {:math => 0.333}})
+    str.add_testing_data(st)
+
+    refute str.has_grade_and_year(st, :asian, 2011)
+    assert str.has_grade_and_year(st, :asian, 2010)
+  end
+
+  def test_it_can_add_testing_data_to_its_repository_when_object_is_new
+    # skip
+    str = StatewideTestRepository.new
+    existing = nil
+    data = {:name => "Colorado", :race => :asian, :year => 2010, :subject => :math, :percent => 0.333}
+    str.add_test_results(data, existing)
+
+    expected = {2010 => {:math => 0.333}}
+    assert_equal expected, str.find_by_name('colorado').race_data(:asian)
+  end
+
+  def test_it_can_add_testing_data_to_its_repository_when_object_race_exists
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new({:name => "Colorado", :asian => {2010 => {:math => 0.333}}})
+    str.add_testing_data(st)
+    existing = st
+    data = {:name => "Colorado", :race => :asian, :year => 2011, :subject => :math, :percent => 0.465}
+    str.add_test_results(data, existing)
+
+    expected = {2010 => {:math => 0.333}, 2011 => {:math => 0.465}}
+    assert_equal expected, str.find_by_name('colorado').race_data(:asian)
+  end
+
+  def test_it_can_add_testing_data_to_its_repository_when_object_race_and_year_exists
+    # skip
+    str = StatewideTestRepository.new
+    st = StatewideTest.new({:name => "Colorado", :asian => {2010 => {:math => 0.333}}})
+    str.add_testing_data(st)
+    existing = st
+    data = {:name => "Colorado", :race => :asian, :year => 2010, :subject => :reading, :percent => 0.465}
+    str.add_test_results(data, existing)
+
+    expected = {2010 => {:math => 0.333, :reading => 0.465}}
+    assert_equal expected, str.find_by_name('colorado').race_data(:asian)
+  end
+
+  def test_it_can_load_and_add_grade_data_from_a_file
+    # skip
+    str = StatewideTestRepository.new
+    str.load_data({:statewide_testing => {:third_grade => "./test/data/3rd_grade_prof_CSAP_TCAP.csv"}})
+    expected = {2010 => {:reading => 0.0}, 2008 => {:writing => 0.0}}
+    assert_equal expected, str.find_by_name('platte valley re-3').grade_data(:third_grade)
+  end
+
+  def test_it_can_load_and_add_test_data_from_a_file
+    skip
+    str = StatewideTestRepository.new
+    str.load_data({:statewide_testing => {:math => "./test/data/Avg_prof_CSAP_TCAP_by_race_Math.csv"}})
+    expected = {2011 => {:math => 0.8169}, 2012 => {:math => 0.8182}, 2013 => {:math => 0.8053}, 2014 => {:math => 0.8}}
+    assert_equal expected, str.find_by_name('academy 20').race_data(:asian)
+  end
+
+end
