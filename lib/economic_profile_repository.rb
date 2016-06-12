cbbf7979e5833e388c29211a4ec1e9c0d5dfe336
Ryan Flach <ryanflach@users.noreply.github.com>
2016-06-12 16:21:32 -0600

    Progress on repository and tests

--- lib/economic_profile_repository.rb
+++ lib/economic_profile_repository.rb
@@ -16,21 +16,114 @@ class EconomicProfileRepository
     econ_profiles[district_name.upcase]
   end
 
+  def base_data(row)
+    {:name => row[:location], :year => row[:timeframe],
+     :type => row[:dataformat], :data => row[:data]}
+  end
+
   def load_data(data_source)
     data_source.values[0].values.each_with_index do |filename, index|
       CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
         data = base_data(row)
         data_type = data_source.values[0].keys[index]
-        existing = find_by_name(data[:name])
-        if grade_levels.include?(data_type)
-          data[:subject], data[:grade] = row[:score].downcase.to_sym, data_type
-          add_grade_data(data, existing)
-        else
-          data[:race] = race_to_sym(row[:race_ethnicity])
-          data[:subject] = data_type
-          add_test_results(data, existing)
+        if data_type == :free_or_reduced_price_lunch
+          next unless row[:poverty_level].strip.downcase.include?('free or')
         end
+        existing = find_by_name(data[:name])
+        check_data_type_and_add_to_repo(data, data_type, existing)
       end
+    end
+  end
+
+  def check_data_type_and_add_to_repo(data, data_type, existing)
+    if data_type == :median_household_income
+      add_median_household_data(data, existing)
+    elsif data_type == :children_in_poverty
+      add_children_in_poverty(data, existing)
+    elsif data_type == :free_or_reduced_price_lunch
+      add_free_or_reduced_lunch(data, existing)
+    else
+      add_title_i(data, existing)
+    end
+  end
+
+  def add_median_household_data(data, existing)
+    income = data[:data].to_i
+    year_range = data[:year].split('-').map {|year| year.to_i}
+    if existing.nil?
+      econ_profile = {:name => data[:name],
+                      :median_household_income => {year_range => income}}
+      add_econ_profile_data(EconomicProfile.new(econ_profile))
+    elsif existing.median_household_income.nil?
+      existing.econ_data[:median_household_income] = {year_range => income}
+    else
+      existing.median_household_income.merge!({year_range => income})
+    end
+  end
+
+  def add_children_in_poverty(data, existing)
+    value = assign_percent_or_num(data)
+    year = data[:year].to_i
+    if value.is_a?(Float)
+      if existing.nil?
+        econ_profile = {:name => data[:name],
+                        :children_in_poverty => {year => value}}
+        add_econ_profile_data(EconomicProfile.new(econ_profile))
+      elsif existing.children_in_poverty.nil?
+        existing.econ_data[:children_in_poverty] = {year => value}
+      else
+        existing.children_in_poverty.merge!({year => value})
+      end
+    end
+  end
+
+  def add_free_or_reduced_lunch(data, existing)
+    value = assign_percent_or_num(data)
+    year = data[:year].to_i
+    econ_profile = {:name => data[:name], :lunch => {}}
+    if value.is_a?(Float)
+      econ_profile[:lunch] = {year => {:percentage => value}}
+      add_lunch_data(econ_profile, existing)
+    else
+      econ_profile[:lunch] = {year => {:total => value}}
+      add_lunch_data(econ_profile, existing)
+    end
+  end
+
+  def add_lunch_data(data, existing)
+    year = data[:lunch].keys[0]
+    if existing.nil?
+      data[:free_or_reduced_price_lunch] = data[:lunch]
+      data.delete(:lunch)
+      add_econ_profile_data(EconomicProfile.new(data))
+    elsif existing.free_or_reduced_price_lunch.nil?
+      existing.econ_data[:free_or_reduced_price_lunch] = data[:lunch]
+    elsif existing.free_or_reduced_price_lunch[year].nil?
+      existing.free_or_reduced_price_lunch.merge!(data[:lunch])
+    else
+      existing.free_or_reduced_price_lunch[year].merge!(data[:lunch][year])
+    end
+  end
+
+  def add_title_i(data, existing)
+    value = data[:data].to_f
+    year = data[:year].to_i
+    if existing.nil?
+      econ_profile = {:name => data[:name],
+                      :title_i => {year => value}}
+      add_econ_profile_data(EconomicProfile.new(econ_profile))
+    elsif existing.title_i.nil?
+      existing.econ_data[:title_i] = {year => value}
+    else
+      existing.title_i.merge!({year => value})
+    end
+  end
+
+  def assign_percent_or_num(data)
+    if data[:type].downcase.strip == "percent"
+      data[:data].to_f
+    else
+      data[:data].to_i
     end
   end
 
--- test/economic_profile_repository_test.rb
+++ test/economic_profile_repository_test.rb
@@ -27,4 +27,176 @@ class EconomicProfileRepositoryTest < Minitest::Test
     assert_equal expected, epr.find_by_name("Colorado")
   end
 
+  def test_it_can_add_median_household_data_to_a_new_econ_object_and_store_in_repo
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2005-2009", :type => "Currency", :data => "50000"}
+    existing = nil
+    epr.add_median_household_data(data, existing)
+
+    expected = {[2005, 2009] => 50000}
+    assert_equal expected, epr.find_by_name("academy 20").median_household_income
+  end
+
+  def test_it_can_add_median_household_data_to_an_existing_econ_object_with_no_existing_median_data
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2005-2009", :type => "Currency", :data => "50000"}
+    existing = EconomicProfile.new({:name => "academy 20"})
+    epr.add_econ_profile_data(existing)
+    epr.add_median_household_data(data, existing)
+
+    expected = {[2005, 2009] => 50000}
+    assert_equal expected, epr.find_by_name("academy 20").median_household_income
+  end
+
+  def test_it_can_add_to_existing_median_household_data
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2008-2012", :type => "Currency", :data => "60000"}
+    existing = EconomicProfile.new({:name => "academy 20", :median_household_income => {[2005, 2009] => 50000}})
+    epr.add_econ_profile_data(existing)
+    epr.add_median_household_data(data, existing)
+
+    expected = {[2005, 2009] => 50000, [2008, 2012] => 60000}
+    assert_equal expected, epr.find_by_name("academy 20").median_household_income
+  end
+
+  def test_it_can_check_data_to_determine_percent_or_number_and_convert_to_int_or_float
+    epr = EconomicProfileRepository.new
+    data_p = {:type => "PERCENT", :data => "0.24252"}
+    data_n = {:type => "number", :data => "245"}
+
+    assert_equal 0.24252, epr.assign_percent_or_num(data_p)
+    assert_equal 245, epr.assign_percent_or_num(data_n)
+  end
+
+  def test_it_will_store_only_poverty_percentage_data
+    epr = EconomicProfileRepository.new
+    data_n = {:name => "Academy 20", :year => "2008", :type => "number", :data => "2424"}
+    data_p = {:name => "Academy 20", :year => "2008", :type => "percent", :data => "0.255"}
+    existing = nil
+
+    assert_nil epr.add_children_in_poverty(data_n, existing)
+
+    epr.add_children_in_poverty(data_p, existing)
+    expected = {2008 => 0.255}
+    assert_equal expected, epr.find_by_name("academy 20").children_in_poverty
+  end
+
+  def test_it_can_add_poverty_data_to_a_new_econ_profile_object_and_store_in_repo
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2008", :type => "percent", :data => "0.255"}
+    existing = nil
+    epr.add_children_in_poverty(data, existing)
+
+    expected = {2008 => 0.255}
+    assert_equal expected, epr.find_by_name("academy 20").children_in_poverty
+  end
+
+  def test_it_can_add_poverty_data_to_an_existing_econ_profile_object_that_lacks_poverty_data
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2008", :type => "percent", :data => "0.255"}
+    existing = EconomicProfile.new({:name => "academy 20"})
+    epr.add_econ_profile_data(existing)
+    epr.add_children_in_poverty(data, existing)
+
+    expected = {2008 => 0.255}
+    assert_equal expected, epr.find_by_name("academy 20").children_in_poverty
+  end
+
+  def test_it_can_add_poverty_data_to_an_existing_econ_profile_with_poverty_data
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2008", :type => "percent", :data => "0.255"}
+    existing = EconomicProfile.new({:name => "academy 20", :children_in_poverty => {2005 => 0.298}})
+    epr.add_econ_profile_data(existing)
+    epr.add_children_in_poverty(data, existing)
+
+    expected = {2005 => 0.298, 2008 => 0.255}
+    assert_equal expected, epr.find_by_name("academy 20").children_in_poverty
+  end
+
+  def test_it_can_add_free_or_reduced_lunch_data_to_a_new_econ_profile_object_and_add_to_repo
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :lunch => {2008 => {:percentage => 0.445}}}
+    existing = nil
+
+    epr.add_lunch_data(data, existing)
+
+    expected = {2008 => {:percentage => 0.445}}
+    assert_equal expected, epr.find_by_name("academy 20").free_or_reduced_price_lunch
+  end
+
+  def test_it_can_add_free_or_reduced_lunch_data_to_an_existing_econ_profile_with_no_lunch_data
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :lunch => {2008 => {:percentage => 0.445}}}
+    existing = EconomicProfile.new({:name => "Academy 20"})
+    epr.add_econ_profile_data(existing)
+    epr.add_lunch_data(data, existing)
+
+    expected = {2008 => {:percentage => 0.445}}
+    assert_equal expected, epr.find_by_name("academy 20").free_or_reduced_price_lunch
+
+    data_n = {:name => "Academy 20", :lunch => {2008 => {:total => 105}}}
+    epr.add_lunch_data(data_n, epr.find_by_name('academy 20'))
+    expected = {2008 => {:percentage => 0.445, :total => 105}}
+    assert_equal expected, epr.find_by_name('academy 20').free_or_reduced_price_lunch
+  end
+
+  def test_it_can_add_free_or_reduced_lunch_data_to_existing_that_lacks_data_for_that_year
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :lunch => {2008 => {:percentage => 0.445}}}
+    existing = EconomicProfile.new({:name => "Academy 20", :free_or_reduced_price_lunch => {2009 => {:percentage => 0.523}}})
+    epr.add_econ_profile_data(existing)
+    epr.add_lunch_data(data, existing)
+
+    expected = {2008 => {:percentage => 0.445}, 2009 => {:percentage => 0.523}}
+    assert_equal expected, epr.find_by_name('academy 20').free_or_reduced_price_lunch
+  end
+
+  def test_it_can_add_title_i_data_to_a_new_econ_profile_object_and_add_to_repo
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2009", :type => "Percent", :data => "0.554"}
+    existing = nil
+    epr.add_title_i(data, existing)
+
+    expected = {2009 => 0.554}
+    assert_equal expected, epr.find_by_name('academy 20').title_i
+  end
+
+  def test_it_can_add_title_i_data_to_an_existing_econ_profile_object_that_lacks_title_i_data
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2009", :type => "Percent", :data => "0.554"}
+    existing = EconomicProfile.new({:name => "Academy 20"})
+    epr.add_econ_profile_data(existing)
+    epr.add_title_i(data, existing)
+
+    expected = {2009 => 0.554}
+    assert_equal expected, epr.find_by_name('academy 20').title_i
+  end
+
+  def test_it_can_add_title_i_data_to_an_existing_econ_profile_object_that_has_title_i_data
+    epr = EconomicProfileRepository.new
+    data = {:name => "Academy 20", :year => "2009", :type => "Percent", :data => "0.554"}
+    existing = EconomicProfile.new({:name => "Academy 20", :title_i => {2008 => 0.546}})
+    epr.add_econ_profile_data(existing)
+    epr.add_title_i(data, existing)
+
+    expected = {2008 => 0.546, 2009 => 0.554}
+    assert_equal expected, epr.find_by_name('academy 20').title_i
+  end
+
+  def test_it_can_load_and_store_median_income_data
+    epr = EconomicProfileRepository.new
+    epr.load_data({:economic_profile => {
+                      :median_household_income => "./test/data/Median_household_income.csv"}})
+    expected = {[2005, 2009] => 56222, [2006, 2010] => 56456, [2008, 2012] => 58244, [2007, 2011] => 57685, [2009, 2013] => 58433}
+    assert_equal expected, epr.find_by_name('colorado').median_household_income
+  end
+
+  def test_it_can_load_and_store_poverty_data
+    epr = EconomicProfileRepository.new
+    epr.load_data({:economic_profile => {
+                      :median_household_income => "./test/data/Median_household_income.csv"}})
+    expected = {[2005, 2009] => 56222, [2006, 2010] => 56456, [2008, 2012] => 58244, [2007, 2011] => 57685, [2009, 2013] => 58433}
+    assert_equal expected, epr.find_by_name('colorado').median_household_income
+  end
+
 end
